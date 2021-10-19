97ca25260e7d8d48ae0320c635ccc27ab07f1a86
Administrator <535260592@qq.com>
2021-10-20 02:29:26 +0800

    test：sql

--- config/test_sql.py
+++ config/test_sql.py
@@ -1,4 +1,21 @@
 import pymysql
 
 #获取连接对象
-conn = pymysql.connect('12')
+conn = pymysql.connect(host="127.0.0.1",
+                       user="root",
+                       passwd="123456",
+                       db="sakila",
+                       charset='utf8')
+
+
+
+cursor = conn.cursor()
+sql = "select * from actor"
+cursor.execute(sql)
+# print(cursor.fetchone())
+print('============================')
+print(cursor.fetchmany(3))
+print('============================')
+print(cursor.fetchall())
+
+conn.close()
