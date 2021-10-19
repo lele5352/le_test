b181fae08663654634f3e27690eca863eb103e6f
lele5352 <535260592@qq.com>
2021-10-19 17:39:42 +0800

    test:commit4

--- config/excel_test.py
+++ config/excel_test.py
@@ -3,5 +3,10 @@ from openpyxl import Workbook
 #with open('excel_test01','w+',encoding='utf-8') as f:
 
 wb = Workbook()
-wb.create_sheet('123')
 print(wb.active)
+wb.create_sheet('123')
+
+print(wb.sheetnames)
+
+wb = wb['123']
+print(wb)
