7bfa99460db9913d637ea5ca09ac7800faaf0ebf
lele5352 <535260592@qq.com>
2021-10-19 19:54:32 +0800

    1.补充notes相关信息

--- config/excel_test.py
+++ config/excel_test.py
@@ -2,11 +2,14 @@ from openpyxl import Workbook
 
 #with open('excel_test01','w+',encoding='utf-8') as f:
 
+#test
+
+#获取工作文档
 wb = Workbook()
 print(wb.active)
 wb.create_sheet('123')
 
 print(wb.sheetnames)
-
+#切换工作表
 wb = wb['123']
 print(wb)
---
+++ config/test_sql.py
@@ -0,0 +1,4 @@
+import pymysql
+
+#获取连接对象
+conn = pymysql.connect('12')
